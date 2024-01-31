fb5c07b040eea7011206f6bd2055c9c75ab0ce7c
ryderhornedesign <ryderhornedev@gmail.com>
2024-01-31 16:52:22 -0700

    Removed unnessecary prop

--- app/shop/products/[id]/page.tsx
+++ app/shop/products/[id]/page.tsx
@@ -20,7 +20,7 @@ export default async function Page({ params }: {params: {
             <p>Loading product...</p>
           }>
             {
-              Product ? <ProductElement cookies={cookies().toString()} product={JSON.stringify(Product)} price={JSON.stringify(await api.stripeRouter.getPriceInformation.query({ priceId: Product.default_price as string }) || "{}")} button="add" /> : <main className="flex flex-col flex-wrap justify-center items-center before:absolute before:inset-0 before:bg-cover before:bg-no-repeat before:bg-center before:h-full before:w-full before:-z-[1] before:shop-bg-image before:brightness-50 text-center p-4 min-[300px]:p-12 md:p-24">
+              Product ? <ProductElement product={JSON.stringify(Product)} price={JSON.stringify(await api.stripeRouter.getPriceInformation.query({ priceId: Product.default_price as string }) || "{}")} button="add" /> : <main className="flex flex-col flex-wrap justify-center items-center before:absolute before:inset-0 before:bg-cover before:bg-no-repeat before:bg-center before:h-full before:w-full before:-z-[1] before:shop-bg-image before:brightness-50 text-center p-4 min-[300px]:p-12 md:p-24">
                 <h2 className="text-4xl mb-4">Product Not Found</h2>
                 <p className="mb-2">We&apos;re awfully sorry, but it seems like we could not find that product. Please try again later.</p>
                 <p className="mb-4">If you think this is a mistake, please <Link href="/contact-us" className="text-yellow-500">contact us</Link> with information about the product you were trying to find. Thank you!</p>
