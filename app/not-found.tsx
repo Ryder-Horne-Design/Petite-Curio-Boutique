2209d04d5fbb9777398756a341652c1cd7f26e0a
ryderhornedesign <ryderhornedev@gmail.com>
2024-02-08 19:54:02 -0700

    Not found page fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,13 +4,11 @@ import PageHeader from "@/components/page-header";
 
 export default function NotFound() {
   return (
-    <main className="text-center p-4 min-[300px]:p-12 md:p-24">
-      <PageHeader src="/images/shop.png" alt="Shop background image">
-        <h2 className="text-4xl mb-4">Error 404</h2>
-        <p className="mb-2">We&apos;re awfully sorry, but it seems like we could not find that page. Please try again later.</p>
-        <p className="mb-4">If you think this is a mistake, please <Link href="/contact-us" className="text-yellow-500">contact us</Link> with information about the page you were trying to find. Thank you!</p>
-        <LinkButton href="/" textContent="Back To Home Page" />
-      </PageHeader>
-    </main>
+    <PageHeader className="p-4 min-[300px]:p-12 md:p-24" src="/images/shop.png" alt="Shop background image">
+      <h2 className="text-4xl mb-4">Error 404</h2>
+      <p className="mb-2">We&apos;re awfully sorry, but it seems like we could not find that page. Please try again later.</p>
+      <p className="mb-4">If you think this is a mistake, please <Link href="/contact-us" className="text-yellow-500">contact us</Link> with information about the page you were trying to find. Thank you!</p>
+      <LinkButton href="/" textContent="Back To Home Page" />
+    </PageHeader>
   );
 };
